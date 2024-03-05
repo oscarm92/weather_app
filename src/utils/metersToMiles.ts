df797e7cda69e61c914dc3461b9cf8aecff7e688
Oscar Morales Jr <oscarmoralesjr@Oscars-MacBook-Pro.local>
2024-03-05 14:32:01 -0800

    second commit

--- src/utils/metersToMiles.ts
+++ src/utils/metersToMiles.ts
@@ -1,5 +1,12 @@
 export function metersToMiles(visibilityInMeters:number):string {
     // 1 meter is approximately 0.000621371 miles
-    const visibilityInMiles = visibilityInMeters * 0.000621371
-  return `${visibilityInMeters.toFixed(0)}mi` //rounfd to 0 decimal places and add 'mi' to unit
+    
+    if (visibilityInMeters==10000) {
+      return "10mi"
+    } 
+    else {
+      const visibilityInMiles = visibilityInMeters * 0.000621371
+      return `${visibilityInMiles.toFixed(0)}mi` //rounfd to 0 decimal places and add 'mi' to unit
+    }
+  
 }
